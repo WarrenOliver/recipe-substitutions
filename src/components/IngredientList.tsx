c78feb93dcacf68f672510af852a3ad301a46521
WarrenOliver <warren@oliver.us>
2023-11-24 22:26:48 -0500

    added next and previous ingredient buttons

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -3,17 +3,20 @@ const IngredientList = ({ ingredients, title }) => {
 
   
   return (
-    <div className="ingredient-list">
-      <h1>{title}</h1>
-      {ingredients.map((ingredient) => (
-        <div className="ingredient-preview" key={ingredient.id}>
-          <Link className="ingredient-links" to={`/ingredients/${ingredient.id}`}>
-            <h2>{ingredient.title}</h2>
-          </Link>
-        </div>
-      ))}
-    </div>
+    <>
+    <h1 className="ingredient-list-title">{title}</h1>
+      <div className="ingredient-list">
+      
+        {ingredients.map((ingredient) => (
+          <div className="ingredient-preview" key={ingredient.id}>
+            <Link className="ingredient-links" to={`/ingredients/${ingredient.id}`}>
+              <h2>{ingredient.title}</h2>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
-export default IngredientList;
+export default IngredientList;  
--- src/components/SingleIngredient.tsx
+++ src/components/SingleIngredient.tsx
@@ -1,10 +1,30 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./UseFetch";
-
+import { useEffect } from "react";
 
 const SingleIngredient = () => {
     const { id } = useParams()
+    const navigate = useNavigate();
     const {data: ingredient, isPending, error} = useFetch('http://localhost:8000/ingredients/'+ id)
+
+    useEffect(() => {
+        
+    }, [id]);
+    
+
+    const goToPrevious = () => {
+        const prevId = parseInt(id, 10) - 1;
+        if (prevId > 0) {
+            navigate(`/ingredients/${prevId}`);
+            window.location.reload();
+        }
+    };
+    
+    const goToNext = () => {
+        const nextId = parseInt(id, 10) + 1;
+        navigate(`/ingredients/${nextId}`);
+        window.location.reload();
+    };
 
     return (
         <div className="single-ingredients-details">
@@ -16,7 +36,10 @@ const SingleIngredient = () => {
                     <div>
                         { ingredient.body }
                     </div>
+                    <button onClick={goToPrevious}>--- Previous Ingredient</button>
+                    <button onClick={goToNext}>Next Ingredient --- </button>
                 </article>
+                
             )}
 
         </div>
